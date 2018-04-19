6be5284471c6e6120ffc3062ccb32370e2c13a09
cs-j <cj@mail.co.de>
2018-04-19 11:31:51 -0400

    Done.

--- reducer.js
+++ reducer.js
@@ -1,5 +1,5 @@
 let state;
-function changeState(state = {count: 0}, action){
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -7,3 +7,21 @@ function changeState(state = {count: 0}, action){
         return state;
     }
   }
+
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+
+function render(){
+  let container = document.getElementById('container');
+  container.textContent = state.count;
+}
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', function() {
+  dispatch({ type: 'INCREASE_COUNT' });
+});
+
+dispatch({ type: '@@INIT' })
